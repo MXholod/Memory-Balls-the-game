2841e026af31535282799194c9f5a7420d2758f4
Michael Holod <git_ara@ukr.net>
2018-12-20 22:57:45 +0200

    Two bottles are draggable to their places for mobile

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,31 @@
 $(function(){
+		let cl1 = document.querySelector(".memory-balls__drop-row-1");
+		let el1 =  cl1.children[0];
 	//Drop zone
-	$(".drop-row-1").droppable({
+	//$(".memory-balls__drop-box-1").droppable({
+	$(el1).droppable({
 		drop:function( event, ui ) {
-			$(this).addClass("dropped").find("p").html("Element was dropped!");
-			//console.log(event," Dropped");
-		}
+			//$(this).addClass("dropped").find("p").html("Element was dropped!");
+		},
+		accept: function(drag) {
+			var dropId = $(this).attr('data-id');
+			var dragId = $(drag).attr('data-id');
+			return dropId === dragId;
+			//console.log("Drag ",$(drag));
+			//console.log("Drop ",$(this));
+		} 
+    });
+	$(".memory-balls__drop-box-2").droppable({
+		drop:function( event, ui ) {
+			//$(this).addClass("dropped").find("p").html("Element was dropped!");
+		},
+		accept: function(drag) {
+			var dropId = $(this).attr('data-id');
+			var dragId = $(drag).attr('data-id');
+			return dropId === dragId;
+			//console.log("Drag ",$(drag));
+			//console.log("Drop ",$(this));
+		} 
     });
 	//Drag elements
 	$("#jar1").draggable({
@@ -15,6 +36,17 @@ $(function(){
 		drag:function(event,ui){
 			//$(this).attr("transform",'matrix(1 0 0 1)'+x+' '+y+')');
 		},
-		//revert:true
+		revert: 'invalid'
+	});
+	//Drag elements
+	$("#jar2").draggable({
+		start:function(event,ui){
+			//event.clientX;
+			//event.clientY;
+		},
+		drag:function(event,ui){
+			//$(this).attr("transform",'matrix(1 0 0 1)'+x+' '+y+')');
+		},
+		revert: 'invalid'
 	});
 });
